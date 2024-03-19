801d453b80dacdecf38402698ecad49c03379fc4
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-03-20 00:24:33 +0300

    Form has added and UI improved

--- generate-pdf.php
+++ generate-pdf.php
@@ -5,11 +5,26 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Dompdf\Dompdf;
+use Dompdf\Options;
 
-$dompdf = new Dompdf;
+$name = $_POST['name'];
+$quantity = $_POST['quantity'];
 
-$dompdf->load_html("Hello World");
+$html = "<h1>Example</h1>";
+$html.="Hello <em>$name</em>";   
+$html.="<img src='example.png' width='200' height='200'>";
+$html.="<p>Quantity: $quantity</p>";
+$options = new Options();
+$options->setChroot(__DIR__);
+$dompdf = new Dompdf($options
+);
+
+$dompdf->load_html($html);
+
+$dompdf->setPaper('A4');
 
 $dompdf->render();
 
-$dompdf->stream();
+$dompdf->add_info('Title', 'Omer'); 
+
+$dompdf->stream('omer.pdf', ["Attachment" => 0]); //I need to install PHP GD library to make it shwo the image in the pdf
